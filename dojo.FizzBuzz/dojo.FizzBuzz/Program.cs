ae4bbdbc7b5efccd514cac35431bd0e092b687ca
CodeTile <43536260+CodeTile@users.noreply.github.com>
2024-05-14 09:23:59 +0100

    Inital Version

---
+++ dojo.FizzBuzz/dojo.FizzBuzz/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace dojo.FizzBuzz
+{
+    internal class Program
+    {
+        private static void Main()
+        {
+            for (int i = 0; i < 100; i++)
+            {
+                if (i % 3 == 0 && i % 5 == 0)
+                    Console.WriteLine("FizzBuzz");
+                else if (i % 5 == 0)
+                    Console.WriteLine("Buzz");
+                else if (i % 3 == 0)
+                    Console.WriteLine("Fizz");
+                else
+                    Console.WriteLine(i);
+            }
+        }
+    }
+}
