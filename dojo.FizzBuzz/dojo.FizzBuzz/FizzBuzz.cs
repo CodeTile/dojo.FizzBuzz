6f7da0c6b54aeb756523a0213bb48de5b95f6a68
CodeTile <43536260+CodeTile@users.noreply.github.com>
2024-05-14 09:28:55 +0100

    Create FizzBuzz class

---
+++ dojo.FizzBuzz/dojo.FizzBuzz/FizzBuzz.cs
@@ -0,0 +1,22 @@
+﻿namespace dojo.FizzBuzz
+{
+    public interface IFizzBuzz
+    {
+        string Parse(int i);
+    }
+
+    public class FizzBuzz : IFizzBuzz
+    {
+        internal string Parse(int i)
+        {
+            if (i % 3 == 0 && i % 5 == 0)
+                return "FizzBuzz";
+            else if (i % 5 == 0)
+                return "Buzz";
+            else if (i % 3 == 0)
+                return "Fizz";
+            else
+                return i.ToString();
+        }
+    }
+}
--- dojo.FizzBuzz/dojo.FizzBuzz/Program.cs
+++ dojo.FizzBuzz/dojo.FizzBuzz/Program.cs
@@ -4,16 +4,10 @@
     {
         private static void Main()
         {
+            var fizzBuzz = new FizzBuzz();
             for (int i = 0; i < 100; i++)
             {
-                if (i % 3 == 0 && i % 5 == 0)
-                    Console.WriteLine("FizzBuzz");
-                else if (i % 5 == 0)
-                    Console.WriteLine("Buzz");
-                else if (i % 3 == 0)
-                    Console.WriteLine("Fizz");
-                else
-                    Console.WriteLine(i);
+                Console.WriteLine(fizzBuzz.Parse(i));
             }
         }
     }
